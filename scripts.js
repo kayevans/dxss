62caff67d9653b5eab7f47a3f78a2a261cca9cdd
Kay Evans-Stocks <kay.stocks@rogers.com>
2020-10-30 18:49:07 -0400

    added scrolling nav bar animations

--- scripts.js
+++ scripts.js
@@ -13,6 +13,23 @@ dxss.init = function(){
     $('.button--cross').on('click', function(){
         $('.nav__menu').removeClass('open');
     })
+
+    // add class to add background colour to nav
+    $(window).scroll(function(){
+        if( $(this).scrollTop() >= 86){
+            $('.nav').addClass('white');
+            $('.nav .wrapper > .logo img').attr('src', './assets/logo-dark.png');
+            $('.nav--mobile .button--ham img').attr('src', './assets/ham--blue.svg');
+            $('.nav__menu .nav__links').addClass('scrollin');
+            $('.button--call').addClass('scrollin');
+        } else {
+            $('.nav').removeClass('white');
+            $('.nav .wrapper > .logo img').attr('src', './assets/logo-light.png');
+            $('.nav--mobile .button--ham img').attr('src', './assets/ham.svg');
+            $('.nav__menu .nav__links').removeClass('scrollin');
+            $('.button--call').removeClass('scrollin');
+        }
+    })
 }
 
 
